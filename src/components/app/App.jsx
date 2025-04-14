3093cc35b0c55602869576ab09756c7e70012029
musikshr <musikshr@mail.ru>
2025-04-14 17:32:14 +0300

    all

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router";
 import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import Catalog from "../catalog/Catalog";
 import Content from "../content/Content";
 import Contacts from "../contacts/Contacts";
@@ -19,6 +20,16 @@ const App = () => {
     const mockData = data;
     setPropertiesData(mockData);
   }, []);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleForceUpdate = () => {
+      navigate(window.location.pathname.replace('/city', ''));
+    };
+
+    window.addEventListener('forceRouteUpdate', handleForceUpdate);
+    return () => window.removeEventListener('forceRouteUpdate', handleForceUpdate);
+  }, [navigate]);
 
   return (
     <div className="App">
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import App from "./components/app/App";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { HashRouter } from 'react-router-dom';
 
+const redirectPath = localStorage.getItem('redirect');
+if (redirectPath) {
+  localStorage.removeItem('redirect');
+  
+  // Ждём загрузки всех скриптов
+  window.addEventListener('load', () => {
+    window.history.replaceState(null, '', redirectPath);
+    
+    // Форсируем обновление роутера
+    const event = new Event('forceRouteUpdate');
+    window.dispatchEvent(event);
+  });
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-setTimeout(() => {
-  const redirectPath = sessionStorage.getItem('redirect');
-  if (redirectPath) {
-    sessionStorage.removeItem('redirect');
-    history.replaceState(null, '', redirectPath);
-  }
-}, 50);
-
 root.render(
   // <React.StrictMode>
     <BrowserRouter basename="/city" >
