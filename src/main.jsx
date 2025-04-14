06ec83b55909a95757951442e22105018e2943e2
musikshr <musikshr@mail.ru>
2025-04-14 15:41:48 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/app/App";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
