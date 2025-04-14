eafd037148eea7c36547b24ffc05a8c29dacdf4c
musikshr <musikshr@mail.ru>
2025-04-14 16:34:07 +0300

    404.html

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app/App";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// const redirect = sessionStorage.getItem('redirect');
+// if (redirect) {
+//   sessionStorage.removeItem('redirect');
+//   window.location.href = redirect;
+// }
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/city" >
       <App />
     </BrowserRouter>
   </React.StrictMode>
