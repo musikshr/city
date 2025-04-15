28a54ecd3b90ee6019c908c35b194632c765c05e
musikshr <musikshr@mail.ru>
2025-04-16 01:19:15 +0300

    main and viteConfg

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  // plugins: [react()],
   build: {
     outDir: 'dist',
   },
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          injectScript: `
+            <script>
+              const redirect = sessionStorage.redirect;
+              delete sessionStorage.redirect;
+              if (redirect && redirect !== location.pathname) {
+                history.replaceState(null, null, redirect);
+              }
+            </script>
+          `
+        }
+      }
+    })
+  ]
 })
