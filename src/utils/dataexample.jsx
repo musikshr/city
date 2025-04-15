a5133dbc3d984cc97436079543585e952b663dcf
musikshr <musikshr@mail.ru>
2025-04-15 14:13:47 +0300

    vite.config.js

--- src/utils/dataexample.jsx
+++ src/utils/dataexample.jsx
@@ -1,11 +1,11 @@
 const data = [
     {
         id: 1,
-        images: ['/images/img5.png', '/images/KammaevaPlan.jpeg'],
+        images: ['/city/images/img5.png', '/city/images/KammaevaPlan.jpeg'],
         nameComplex: 'Алые Паруса',
         relinquishment: '4кв 2026',
         dataFlat: [
-            { id: 1, map: '3Af7a9e58994e07c4260eed3a826069f5e1a3dc5972843cf80223779c0ae47442a', img: '/images/img4.jpg', img2: '/images/KammaevaPlan.jpeg', adress: 'Подмосковье, д. Примерное', price: 25000000, area: 250, rooms: 10, floor: '7/14', street: 'Алые Паруса' },
+            { id: 1, map: '3Af7a9e58994e07c4260eed3a826069f5e1a3dc5972843cf80223779c0ae47442a', img: '/city/images/img4.jpg', img2: '/city/images/KammaevaPlan.jpeg', adress: 'Подмосковье, д. Примерное', price: 25000000, area: 250, rooms: 10, floor: '7/14', street: 'Алые Паруса' },
             { id: 2, map: '3Af7a9e58994e07c4260eed3a826069f5e1a3dc5972843cf80223779c0ae47442a', img: '/images/img4.jpg', img2: '/images/img5.png', adress: 'Москва, ул. Центральная 10', price: 15000000, area: 75, rooms: 7, floor: '7/14', street: 'Перспектива' },
             { id: 3, map: '3Af7a9e58994e07c4260eed3a826069f5e1a3dc5972843cf80223779c0ae47442a', img: '/images/img4.jpg', img2: '/images/img5.png', adress: 'Санкт-Петербург, ул Деловая 5', price: 25000000, area: 100, rooms: 3, floor: '7/14', street: 'Примакова' },
             { id: 4, map: '3Af7a9e58994e07c4260eed3a826069f5e1a3dc5972843cf80223779c0ae47442a', img: '/images/img4.jpg', img2: '/images/img5.png', adress: 'Подмосковье, д. Примерное', price: 50000000, area: 200, rooms: 13, floor: '7/14', street: 'Примакова' },
