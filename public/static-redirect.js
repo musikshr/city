1364f226a6225d655f65bc289b88df89533523ba
musikshr <musikshr@mail.ru>
2025-04-16 02:22:26 +0300

    main and viteConfg

--- public/static-redirect.js
+++
@@ -1,8 +0,0 @@
-// public/static-redirect.js
-document.addEventListener('DOMContentLoaded', () => {
-    const redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect !== location.pathname) {
-      history.replaceState(null, null, redirect);
-    }
-  });
