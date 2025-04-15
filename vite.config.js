0f1a92dc290ebf0214c89af697378bd31a54dc69
musikshr <musikshr@mail.ru>
2025-04-16 01:36:34 +0300

    main and viteConfg

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './public/index.html'  // Указываем использовать ваш файл
-      }
-    }
-  }
 })
