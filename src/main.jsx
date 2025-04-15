33574f91d0b24a3eb049b4650e8223af58c86510
musikshr <musikshr@mail.ru>
2025-04-16 02:12:00 +0300

    main and viteConfg

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ if (redirectPath) {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/">
     {/* <HashRouter> */}
       <App />
     {/* </HashRouter> */}
