c3258fc17a179517d67326e46fbfd5d166f1a62a
musikshr <musikshr@mail.ru>
2025-04-15 23:55:20 +0300

    main and viteConfg

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ if (redirectPath) {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename="/citygroup05.ru" >
+    <BrowserRouter basename="/city" >
       <App />
     </BrowserRouter>
   // </React.StrictMode>
